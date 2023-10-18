60c7fbcb6bb4a015ad366451954a7c69ec9cb927
x4v13r100 <j4v13r15@hotmail.com>
2023-10-18 17:17:40 -0500

    Create trama for up to DB

--- spot.py
+++ spot.py
@@ -2,6 +2,10 @@ import geopandas as gpd
 from shapely.geometry import Polygon, Point
 import pandas as pd
 
+
+#! Se necesita el poligono.csv
+#! conseguir los datosde la tabla puntos lotes => lat,lng
+
 # Carga el polígono desde el archivo CSV
 polygon_csv = "./file/poligono.csv"
 polygon_df = pd.read_csv(polygon_csv)
@@ -22,23 +26,31 @@ points = []
 x_min, y_min, x_max, y_max = polygon.bounds
 
 x = x_min
+line = 1
+point_number = 1
+
 while x <= x_max:
     y = y_min
     while y <= y_max:
         point = Point(x, y)
+        
         if point.within(polygon):
-            points.append(point)
+            points.append((point, line, point_number))
+            point_number += 1
+            
         y += distance_between_points
     x += distance_between_points
+    line += 1
+    point_number = 1
 
 # Extrae las coordenadas de los puntos en formato (latitud, longitud)
-point_coords = [(point.x, point.y) for point in points]
+point_coords = [(point.x, point.y, line, point_number) for point, line, point_number in points]
 
 # Crea un DataFrame de pandas para los puntos
-points_df = pd.DataFrame(point_coords, columns=['lat', 'lng'])
+points_df = pd.DataFrame(point_coords, columns=['lat', 'lng', 'linea', 'posicion'])
 
 # Guarda el DataFrame en un archivo CSV
-points_csv = "puntos_generados.csv"
+points_csv = "./results/puntos_generados.csv"
 points_df.to_csv(points_csv, index=False)
 
 print(f"Se han generado puntos ordenados con una separación de 2 metros dentro del polígono y se han guardado en '{points_csv}'.")
--- trama.py
+++ trama.py
@@ -2,31 +2,39 @@ import pandas as pd
 import random
 from datetime import datetime
 
-puntos_df = pd.read_csv('./puntos_generados.csv')
+#! Trasforma los datos de spot.py
+#
+puntos_df = pd.read_csv('./results/puntos_generados.csv')
 
 finca_id = 367
 lote_id = 1
 
 fecha = datetime.now().strftime('%Y-%m-%d')
 
+def generar_sufijo():
+    return str(random.randint(1000, 9999))
+
+def nombre_planta(posicion):
+    return 'AGPL47L' + str(posicion)
+
 nuevo_df = pd.DataFrame({
-    'nombre_spot': ['Spot1', 'Spot2'],  # Cambia estos nombres si lo deseas
+    'nombre_spot': ['AGPL47L' + generar_sufijo() for _ in range(len(puntos_df))],
     'lat': puntos_df['lat'],
     'lng': puntos_df['lng'],
     'lote_id': lote_id,
-    'linea': random.randint(1, 10),
-    'posicion': random.randint(1, 10),
-    'tipo_poligono_id': random.randint(1, 10),
-    'distancia': random.randint(1, 100),
-    'distancia_2': random.randint(1, 100),
-    'spot_id': random.randint(1, 1000),
-    'nombre_planta': ['Planta1', 'Planta2'],  # Cambia estos nombres si lo deseas
-    'fecha_siembra': fecha,
-    'tipo_variedad_id': 5,
-    'tipo_riego_id': 1
-    'finca_id': finca_id
+    'linea': puntos_df['linea'],
+    'posicion': puntos_df['posicion'],
+    'tipo_poligono_id': [1] * len(puntos_df),
+    'distancia': [10] * len(puntos_df),
+    'distancia_2': "",
+    'spot_id': list(range(1, len(puntos_df) + 1)),
+    'nombre_planta': puntos_df['posicion'].apply(nombre_planta),
+    'fecha_siembra': [fecha] * len(puntos_df),
+    'tipo_variedad_id': [5] * len(puntos_df),
+    'tipo_riego_id': [1] * len(puntos_df),
+    'finca_id': [finca_id] * len(puntos_df) 
 })
 
-nuevo_df.to_csv('./spots.csv', index=False)
+nuevo_df.to_csv('./results/subir_spots.csv', index=False, sep=';')
 
 print("Se han generado spots y se han guardado en 'spots.csv'")
